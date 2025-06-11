c0d37b1cb438f637f2a6924e621afece10cdc9a2
Gamer <gamerxyzmaster@gmail.com>
2025-06-11 20:08:23 +0200

    Add files via upload

--- web.js
+++ web.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("title").classList.toggle("active")
+    document.getElementById("desc").classList.toggle("active")
+
     const menubtn = document.getElementById("menubtn")
     const menu = document.getElementById("menu")
 
     menubtn.addEventListener("click", function() {
         menu.classList.toggle("active")
+        menubtn.textContent = menu.classList.contains("active") ? "notes" : "menu"
     })
 })
