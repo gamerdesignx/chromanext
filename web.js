1134be15092f1834616d917a273b7bb39a71e2dd
Gamer <gamerxyzmaster@gmail.com>
2025-06-23 22:09:27 +0200

    Add files via upload

--- web.js
+++ web.js
@@ -1,16 +1,3 @@
-// Animations
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("title").classList.toggle("active")
-    document.getElementById("desc").classList.toggle("active")
-
-    const menubtn = document.getElementById("menubtn")
-    const menu = document.getElementById("menu")
-
-    const blur = document.getElementById("blur")
-
-    menubtn.addEventListener("click", function() {
-        menu.classList.toggle("active")
-
-        blur.classList.toggle("active")
-    })
+    
 })
